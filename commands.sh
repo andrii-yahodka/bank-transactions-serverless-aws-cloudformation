03806f395f5abe6bc03950f044ec10d462ca513c
Andrii Yahodka <andrei.iagodka@gmail.com>
2024-04-30 02:20:34 +0300

    Integrate GET request to DynamoDb

--- commands.sh
+++ commands.sh
@@ -6,3 +6,6 @@ aws cloudformation deploy --template-file api_gateway_template.packaged.yaml --s
 
 aws cloudformation package --template-file lambda_function_template.yaml --s3-bucket serverless-payment-service --output-template-file lambda_function_template.packaged.yaml
 aws cloudformation deploy --template-file lambda_function_template.packaged.yaml --stack-name ServerlessPaymentServiceStack --capabilities=CAPABILITY_NAMED_IAM
+
+aws cloudformation package --template-file api_gateway_v2_template.yaml --s3-bucket serverless-payment-service --output-template-file api_gateway_v2_template.packaged.yaml
+aws cloudformation deploy --template-file api_gateway_v2_template.packaged.yaml --stack-name ClientsStack --capabilities=CAPABILITY_NAMED_IAM
