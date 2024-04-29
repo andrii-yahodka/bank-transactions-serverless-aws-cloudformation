c78d0ecdb9c4983dd72169cb486f4d0be21fead5
Andrii Yahodka <andrei.iagodka@gmail.com>
2024-04-29 23:37:30 +0300

    Introduce clients API Gateway with DynamoDb template

--- commands.sh
+++ commands.sh
@@ -1,2 +1,5 @@
 aws cloudformation package --template-file dynamodb_template.yaml --s3-bucket serverless-payment-service --output-template-file dynamodb_template.packaged.yaml
 aws cloudformation deploy --template-file dynamodb_template.packaged.yaml --stack-name ServerlessPaymentServiceStack --capabilities=CAPABILITY_NAMED_IAM
+
+aws cloudformation package --template-file api_gateway_template.yaml --s3-bucket serverless-payment-service --output-template-file api_gateway_template.packaged.yaml
+aws cloudformation deploy --template-file api_gateway_template.packaged.yaml --stack-name ClientsStack --capabilities=CAPABILITY_NAMED_IAM
